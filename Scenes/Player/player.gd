b21eb7ef0e37429ce182878f00383cf8497970b9
Yadovitiy <yadovitiy@github.com>
2026-06-30 22:34:31 +0300

    Player теперь отскакивает

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -4,14 +4,19 @@ extends CharacterBody3D
 class_name Player
 
 const GRAVITY: float = 15.0
+const JUMP_FORCE: float = 5.0
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-	
+
+
 	
 func _physics_process(delta: float) -> void:
-	velocity.y -= GRAVITY * delta
+	if is_on_floor():
+		velocity.y = JUMP_FORCE
+	else:
+		velocity.y -= GRAVITY * delta
 	move_and_slide()
